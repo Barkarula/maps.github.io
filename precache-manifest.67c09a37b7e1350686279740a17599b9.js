0148c23aa878793e5648ab1c6b77a6a9825a2032
Barkarula <dunaev.daniel@bk.ru>
2020-01-28 23:28:33 +0300

    Updates

---
+++ precache-manifest.67c09a37b7e1350686279740a17599b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18965a8850c54aada036719b4f317f1",
+    "url": "/maps.github.io/index.html"
+  },
+  {
+    "revision": "9638def6b47eeeb47cc7",
+    "url": "/maps.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1e5579dce17cc9e81916",
+    "url": "/maps.github.io/static/js/2.5ec3b5d1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/maps.github.io/static/js/2.5ec3b5d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9638def6b47eeeb47cc7",
+    "url": "/maps.github.io/static/js/main.44f473b8.chunk.js"
+  },
+  {
+    "revision": "6ed317f1479b8ba718c4",
+    "url": "/maps.github.io/static/js/runtime-main.e477016d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/maps.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
